0f30c84dd204993bdef872dfbb5bbbc1af62098c
Nemanja Vasiljevic <xvasil03@gmail.com>
2022-03-06 15:11:55 +0100

    issue_9 - payload/event structure class/interface

--- src/eventnotifications.py
+++ src/eventnotifications.py
@@ -19,71 +19,13 @@ import swift.common.middleware.event_notifications.filter_rules as filter_rules_
 from swift.common.middleware.event_notifications.utils import get_s3_event_name
 
 from swift.common.middleware.event_notifications.configuration import S3ConfigurationValidator, S3NotifiationConfiguration
+from swift.common.middleware.event_notifications.payload import S3NotificationPayload
 
-def create_s3_event(req, app):
-    version, account, container, object = split_path(req.environ['PATH_INFO'], 1, 4, rest_with_last=True)
-    method = req.environ.get('swift.orig_req_method', req.request.method)
-    object_vesion_id = req.headers.get("X-Object-Version-Id", '')
-    sequencer = ""
-    if method in ["PUT", "DELETE"]:
-        if object_vesion_id:
-            # version_id has more accurate timestamp
-            sequencer = object_vesion_id
-        elif 'Last-Modified' in req.headers:
-            sequencer = time.mktime(parsedate(req.headers['Last-Modified']))
-    event = {
-        "Records": {
-            "eventVersion": "2.2",
-            "eventSource": "aws:s3",
-            "eventTime": datetime.now().isoformat(), # todo: check if req has timestamp
-            "eventName": get_s3_event_name(account, container, object, method),
-            "userIdentity":{
-                "principalId": req.environ.get("REMOTE_USER")
-            },
-            "requestParameters":{
-                "sourceIPAddress": req.environ.get("REMOTE_ADDR")
-            },
-            "responseElements":{
-                "x-amz-request-id": req.environ.get("swift.trans_id")
-                # todo: x-amz-host-id
-            },
-            "s3":{
-                "s3SchemaVersion": "1.0",
-                "configurationId": "todo",
-                "bucket":{
-                    "name": container,
-                    "ownerIdentity":{
-                        "principalId": account
-                    },
-                "arn": "arn:aws:s3:::" + container,
-                },
-                "object":{
-                    "key": object,
-                    "size": req.headers.get('content-length', 0),
-                    "eTag": req.headers.get("etag", ''),
-                    "versionId": object_vesion_id,
-                    "sequencer": sequencer
-                }
-            }
-        }
-    }
-    return event
-
-def create_s3_test_event(req):
-    version, account, container, object = split_path(req.environ['PATH_INFO'], 1, 4, rest_with_last=True)
-    event = {
-        "Service": "Amazon S3",
-        "Event": "s3:TestEvent",
-        "Time": datetime.now().isoformat(),
-        "Bucket": container,
-        "RequestId": req.environ.get("swift.trans_id"),
-        "HostId":"TODO"
-    }
-    return event
 
 class EventNotificationsMiddleware(WSGIContext):
     def __init__(self, app, conf):
         self.app = app
+        self.conf = conf
         self.logger = get_logger(conf, log_route='eventnotifications')
         self.configuration_validator = S3ConfigurationValidator('/usr/local/src/swift/swift/common/middleware/event_notifications/configuration-schema.json')
         super(EventNotificationsMiddleware, self).__init__(app)
@@ -110,7 +52,8 @@ class EventNotificationsMiddleware(WSGIContext):
 
         if not resp.headers.get("X-Backend-EventNotification-Ignore"):
             if event_configation_changed:
-                c.put(str(create_s3_test_event(resp)))
+                s3_payload = S3NotificationPayload(self.conf)
+                c.put(json.dumps(s3_payload.create_test_payload(self.app, resp)))
             else:
                 container = get_container_info(resp.environ, self.app)
                 event_notifications_configuration = container.get("sysmeta", {}).get("notifications")
@@ -118,7 +61,8 @@ class EventNotificationsMiddleware(WSGIContext):
                     try:
                         s3_configuration = S3NotifiationConfiguration(event_notifications_configuration)
                         if s3_configuration.get_satisfied_destinations(self.app, resp):
-                            c.put(json.dumps(self.__create_payload(resp)))
+                            s3_payload = S3NotificationPayload(self.conf)
+                            c.put(json.dumps(s3_payload.create_payload(self.app, resp)))
                     except Exception as e:
                         c.put(str(e))
 
