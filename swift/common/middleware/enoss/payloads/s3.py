1a5ae6d713be3a70fdb1b333a57dd7ab27a04a1b
Nemanja Vasiljevic <xvasil03@gmail.com>
2022-04-06 14:59:03 +0000

    issue_5 change of s3 payload structure in order to be compatible with minio bucket events

--- swift/common/middleware/enoss/payloads/s3.py
+++ swift/common/middleware/enoss/payloads/s3.py
@@ -10,6 +10,9 @@ class S3Payload(IPayload):
 
     def create_test_payload(self, app, request, invoking_configuration):
         version, account, container, object = split_path(request.environ['PATH_INFO'], 1, 4, rest_with_last=True)
+        container = container if isinstance(container, str) else ''
+        account = account if isinstance(account, str) else ''
+
         event = {
             "Service": "Amazon S3",
             "Event": "s3:TestEvent",
@@ -18,10 +21,18 @@ class S3Payload(IPayload):
             "RequestId": request.environ.get("swift.trans_id"),
             "HostId":"TODO"
         }
+        if container:
+            event["Bucket"] = container
+        else:
+            event["Account"] = account
         return event
 
     def create_payload(self, app, request, invoking_configuration):
         version, account, container, object = split_path(request.environ['PATH_INFO'], 1, 4, rest_with_last=True)
+        object = object if isinstance(object, str) else ''
+        container = container if isinstance(container, str) else ''
+        account = account if isinstance(account, str) else ''
+
         method = request.environ.get('swift.orig_req_method', request.request.method)
         object_vesion_id = request.headers.get("X-Object-Version-Id", '')
         sequencer = ""
@@ -32,9 +43,9 @@ class S3Payload(IPayload):
             elif 'Last-Modified' in request.headers:
                 sequencer = time.mktime(parsedate(request.headers['Last-Modified']))
         notification_payload = {
-            "Records": {
+            "Records": [{
                 "eventVersion": "2.2",
-                "eventSource": "aws:s3",
+                "eventSource": "swift:s3",
                 "eventTime": datetime.now().isoformat(), # todo: check if req has timestamp
                 "eventName": get_s3_event_name(account, container, object, method),
                 "userIdentity":{
@@ -59,12 +70,12 @@ class S3Payload(IPayload):
                     },
                     "object":{
                         "key": object,
-                        "size": request.headers.get('content-length', 0),
-                        "eTag": request.headers.get("etag", ''),
-                        "versionId": object_vesion_id,
+                        "size": request.headers.get('content-length', 0) if object else 0,
+                        "eTag": request.headers.get("etag", '') if object else '',
+                        "versionId": object_vesion_id if object else '',
                         "sequencer": sequencer
                     }
                 }
-            }
+            }]
         }
         return notification_payload
