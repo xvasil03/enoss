3e363c1cf7dbbb902ae7022b03c24b66a4f38d43
Nemanja Vasiljevic <xvasil03@gmail.com>
2022-04-12 12:13:06 +0000

    issue_13 - functional test - post invalid config, post/get ACL tests

--- test/functional/test_enoss.py
+++ test/functional/test_enoss.py
@@ -22,10 +22,12 @@ import json
 import unittest
 from uuid import uuid4
 
-from test.functional import check_response, retry, load_constraint, SkipTest
+from test.functional import check_response, retry, load_constraint, SkipTest, \
+    requires_acls
 import test.functional as tf
 
 import six
+from copy import deepcopy
 
 
 def setUpModule():
@@ -61,6 +63,24 @@ class TestENOSS(unittest.TestCase):
         self.max_meta_name_length = load_constraint('max_meta_name_length')
         self.max_meta_overall_size = load_constraint('max_meta_overall_size')
         self.max_meta_value_length = load_constraint('max_meta_value_length')
+
+        self.s3_conf_good = {
+            "BeanstalkdConfigrations": [
+                {
+                    "Id": "test",
+                    "Events": ["*"],
+                    "PayloadStructure": "s3",
+                    "Filter": {
+                        "Key": {
+                            "FilterRules": [{
+                                "Name": "suffix",
+                                "Value": ".jpg"
+                            }]
+                        }
+                    }
+                }
+            ]
+        }
 
     def tearDown(self):
         if tf.skip:
@@ -112,55 +132,215 @@ class TestENOSS(unittest.TestCase):
             # retry despite the request having been successfully processed.
             self.assertIn(resp.status, (204, 404))
 
-    def test_post_get_config(self):
+    def post_account(self, url, token, parsed, conn, headers):
+        new_headers = dict({'X-Auth-Token': token}, **headers)
+        conn.request('POST', parsed.path, '', new_headers)
+        return check_response(conn)
+
+    def post_notification(self, url, token, parsed, conn, body=''):
+        path = parsed.path + '/' + self.name + "?notification"
+        headers = {'X-Auth-Token': token,
+                   'Content-Type': 'application/json'}
+        body = json.dumps(body) if body and isinstance(body, dict) else body
+        conn.request('POST', path, body, headers)
+        return check_response(conn)
+
+    def get_notification(self, url, token, parsed, conn):
+        path = parsed.path + '/' + self.name + "?notification"
+        headers = {'X-Auth-Token': token}
+        conn.request('GET', path, '', headers)
+        return check_response(conn)
+
+    def initial_state(self):
+        resp = retry(self.post_notification)
+        self.assertIn(resp.status, (200, 204))
+        resp = retry(self.get_notification)
+        self.assertIn(resp.status, (200, 204))
+        self.assertFalse(resp.text)
+
+    def test_POST_GET_GOOD_config(self):
+        if tf.skip:
+            raise SkipTest
+        pass
+
+        # store notification config
+        resp = retry(self.post_notification, self.s3_conf_good)
+        self.assertIn(resp.status, (200, 204))
+        resp = retry(self.get_notification)
+        self.assertIn(resp.status, (200, 204))
+        self.assertTrue(self.s3_conf_good.items() <= resp.json().items())
+
+        # delete notification config
+        resp = retry(self.post_notification)
+        self.assertIn(resp.status, (200, 204))
+        resp = retry(self.get_notification)
+        self.assertIn(resp.status, (200, 204))
+        self.assertFalse(resp.text)
+
+    def test_POST_GET_BAD_GOD_config(self):
+        pass
         if tf.skip:
             raise SkipTest
 
-        def post_notification(url, token, parsed, conn, body):
-            path = parsed.path + '/' + self.name + "?notification"
-            headers = {'X-Auth-Token': token,
-                       'Content-Type': 'application/json'}
-            body = json.dumps(body) if body else ''
-            conn.request('POST', path, body, headers)
-            return check_response(conn)
-
-        def get_notification(url, token, parsed, conn):
-            path = parsed.path + '/' + self.name + "?notification"
-            headers = {'X-Auth-Token': token}
-            conn.request('GET', path, '', headers)
-            return check_response(conn)
-
-        s3_conf = {
-            "BeanstalkdConfigrations": [
-                {
-                    "Id": "test",
-                    "Events": ["*"],
-                    "PayloadStructure": "s3",
-                    "Filter": {
-                        "Key": {
-                            "FilterRules": [{
-                                "Name": "suffix",
-                                "Value": ".jpg"
-                            }]
-                        }
-                    }
-                }
-            ]
-        }
-
-        # store notification config
-        resp = retry(post_notification, s3_conf)
-        self.assertIn(resp.status, (200, 204))
-        resp = retry(get_notification)
-        self.assertIn(resp.status, (200, 204))
-        self.assertTrue(s3_conf.items() <= resp.json().items())
-
-        # delete notification config
-        resp = retry(post_notification, None)
-        self.assertIn(resp.status, (200, 204))
-        resp = retry(get_notification)
+        # missing mandatory information
+        s3_conf_bad = deepcopy(self.s3_conf_good)
+        del s3_conf_bad["BeanstalkdConfigrations"][0]["Events"]
+        resp = retry(self.post_notification, s3_conf_bad)
+        self.assertEqual(resp.status, 400)
+        resp = retry(self.get_notification)
         self.assertIn(resp.status, (200, 204))
         self.assertFalse(resp.text)
+
+        # invalid/unsupported event type
+        s3_conf_bad["BeanstalkdConfigrations"][0]["Events"] = ["InvalidType"]
+        resp = retry(self.post_notification, s3_conf_bad)
+        self.assertEqual(resp.status, 400)
+        resp = retry(self.get_notification)
+        self.assertIn(resp.status, (200, 204))
+        self.assertFalse(resp.text)
+
+        # invalid json config
+        invalid_json_str = "{test: 1}"
+        resp = retry(self.post_notification, invalid_json_str)
+        self.assertEqual(resp.status, 400)
+        resp = retry(self.get_notification)
+        self.assertIn(resp.status, (200, 204))
+        self.assertFalse(resp.text)
+
+        # if config is invalid it should not update stored config in swift
+        resp = retry(self.post_notification, self.s3_conf_good)
+        self.assertIn(resp.status, (200, 204))
+        resp = retry(self.get_notification)
+        self.assertIn(resp.status, (200, 204))
+        self.assertTrue(self.s3_conf_good.items() <= resp.json().items())
+        stored_config = resp.json()
+        resp = retry(self.post_notification, s3_conf_bad)
+        self.assertEqual(resp.status, 400)
+        resp = retry(self.get_notification)
+        self.assertIn(resp.status, (200, 204))
+        self.assertEqual(stored_config, resp.json())
+
+        # return to initial state
+        self.initial_state()
+
+    @requires_acls
+    def test_acl_get_config(self):
+        # store notification config
+        resp = retry(self.post_notification, self.s3_conf_good)
+        self.assertIn(resp.status, (200, 204))
+        resp = retry(self.get_notification)
+        self.assertIn(resp.status, (200, 204))
+        self.assertTrue(self.s3_conf_good.items() <= resp.json().items())
+
+        # cannot read config
+        resp = retry(self.get_notification, use_account=3)
+        resp.read()
+        self.assertEqual(resp.status, 403)
+
+        # set read acl
+        acl_user = tf.swift_test_user[2]
+        acl = {'read-only': [acl_user]}
+        headers = {'x-account-access-control': json.dumps(acl)}
+        resp = retry(self.post_account, headers=headers, use_account=1)
+        resp.read()
+        self.assertEqual(resp.status, 204)
+
+        # can read config with read-only acl
+        resp = retry(self.get_notification, use_account=3)
+        self.assertIn(resp.status, (200, 204))
+        self.assertTrue(self.s3_conf_good.items() <= resp.json().items())
+
+        # set read-write acl
+        acl_user = tf.swift_test_user[2]
+        acl = {'read-write': [acl_user]}
+        headers = {'x-account-access-control': json.dumps(acl)}
+        resp = retry(self.post_account, headers=headers, use_account=1)
+        resp.read()
+        self.assertEqual(resp.status, 204)
+
+        # can read config with read-write acl
+        resp = retry(self.get_notification, use_account=3)
+        self.assertIn(resp.status, (200, 204))
+        self.assertTrue(self.s3_conf_good.items() <= resp.json().items())
+
+        # delete notification config
+        resp = retry(self.post_notification, use_account=3)
+        resp.read()
+        self.assertIn(resp.status, (200, 204))
+        resp = retry(self.get_notification, use_account=3)
+        self.assertIn(resp.status, (200, 204))
+        self.assertFalse(resp.text)
+
+        # return to initial state
+        self.initial_state()
+
+    @requires_acls
+    def test_acl_post_config(self):
+
+        # no acl at all
+        resp = retry(self.post_notification, self.s3_conf_good, use_account=3)
+        resp.read()
+        self.assertEqual(resp.status, 403)
+        resp = retry(self.get_notification)
+        self.assertIn(resp.status, (200, 204))
+        self.assertFalse(resp.text)
+
+        # set read acl
+        acl_user = tf.swift_test_user[2]
+        acl = {'read-only': [acl_user]}
+        headers = {'x-account-access-control': json.dumps(acl)}
+        resp = retry(self.post_account, headers=headers, use_account=1)
+        resp.read()
+        self.assertEqual(resp.status, 204)
+
+        # read acl still cannot post notification config
+        resp = retry(self.post_notification, self.s3_conf_good, use_account=3)
+        resp.read()
+        self.assertEqual(resp.status, 403)
+        resp = retry(self.get_notification)
+        self.assertIn(resp.status, (200, 204))
+        self.assertFalse(resp.text)
+
+        # set read-write acl
+        acl_user = tf.swift_test_user[2]
+        acl = {'read-write': [acl_user]}
+        headers = {'x-account-access-control': json.dumps(acl)}
+        resp = retry(self.post_account, headers=headers, use_account=1)
+        resp.read()
+        self.assertEqual(resp.status, 204)
+
+        # read-write can post notification config
+        resp = retry(self.post_notification, self.s3_conf_good, use_account=3)
+        resp.read()
+        self.assertIn(resp.status, (200, 204))
+        resp = retry(self.get_notification)
+        self.assertIn(resp.status, (200, 204))
+        self.assertTrue(self.s3_conf_good.items() <= resp.json().items())
+
+        # delete notification config
+        resp = retry(self.post_notification, use_account=3)
+        resp.read()
+        self.assertIn(resp.status, (200, 204))
+        resp = retry(self.get_notification, use_account=3)
+        self.assertIn(resp.status, (200, 204))
+        self.assertFalse(resp.text)
+
+        # remove all acl
+        headers = {'x-account-access-control': json.dumps({})}
+        resp = retry(self.post_account, headers=headers, use_account=1)
+        resp.read()
+        self.assertEqual(resp.status, 204)
+
+        # no acl => no store
+        resp = retry(self.post_notification, self.s3_conf_good, use_account=3)
+        resp.read()
+        self.assertEqual(resp.status, 403)
+        resp = retry(self.get_notification)
+        self.assertIn(resp.status, (200, 204))
+        self.assertFalse(resp.text)
+
+        # return to initial state
+        self.initial_state()
 
 
 if __name__ == '__main__':
